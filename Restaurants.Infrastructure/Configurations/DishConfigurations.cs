0c392bf12407e1ed4d7c610b413741074013f55d
Mostafa Labib <mostafal@ejada.com>
2025-04-05 22:47:08 +0200

    Add Configuration for Entities

---
+++ Restaurants.Infrastructure/Configurations/DishConfigurations.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Restaurants.Domain.Entities;
+
+namespace Restaurants.Infrastructure.Configurations;
+
+public class DishConfigurations:IEntityTypeConfiguration<Dish>
+{
+    public void Configure(EntityTypeBuilder<Dish> builder)
+    {
+        builder.ToTable("Dishes");
+        
+        builder.HasKey(d => d.Id);
+        
+        builder.Property(x => x.Name)
+            .IsRequired()
+            .HasMaxLength(256);
+        builder.Property(x => x.Description)
+            .IsRequired()
+            .HasMaxLength(512);
+    }
+}
