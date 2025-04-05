ca53cf9e6b40e1c676b4815ae5123995241f5586
Mostafa Labib <mostafal@ejada.com>
2025-04-05 22:51:03 +0200

    Add necessary dotnet file

--- Restaurants.Application/obj/Debug/net8.0/Restaurants.Application.AssemblyInfo.cs
+++ Restaurants.Application/obj/Debug/net8.0/Restaurants.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Restaurants.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2967cd793f8b2efe6f4ac5fd8d4bbea341464fe1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Restaurants.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Restaurants.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
