6c6e1078b237fb8f7e01fc034ebd2a7564de8e92
Mostafa Labib <mostafal@ejada.com>
2025-04-05 22:47:38 +0200

    Create Dbcontext.

---
+++ Restaurants.Infrastructure/Persistance/RestaurantDb.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Restaurants.Domain.Entities;
+
+namespace Restaurants.Infrastructure.Persistance;
+
+internal class RestaurantDb(DbContextOptions<RestaurantDb> options) : DbContext(options)
+{
+    internal DbSet<Restaurant> Restaurants { get; set; }
+    internal DbSet<Dish> Dishes { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(GetType().Assembly);
+    }
+}
