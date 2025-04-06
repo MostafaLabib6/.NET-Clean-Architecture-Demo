c73363b9ea3144b3143c426a48f4b05e7a0a18be
Mostafa Labib <mostafal@ejada.com>
2025-04-06 23:42:02 +0200

    Add Restaurant Controller to expose restaurant resourses.

---
+++ Restaurants.API/Controllers/RestaurantsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Restaurants.Application.Services;
+
+namespace Restaurants.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class RestaurantsController(IRestaurantsService restaurantsService) : ControllerBase
+{
+    [HttpGet]
+    public IActionResult GetAll()
+    {
+        var restaurants = restaurantsService.GetAllAsync();
+        return Ok(restaurants);
+    }
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetById(int id)
+    {
+        var restaurant = await restaurantsService.GetByIdAsync(id);
+        if (restaurant == null)
+        {
+            return NotFound();
+        }
+        return Ok(restaurant);
+    }
+}
