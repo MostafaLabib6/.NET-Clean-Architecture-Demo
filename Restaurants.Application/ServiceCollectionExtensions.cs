d040828c9d6b49cc5e76277013070fce3c756d04
Mostafa Labib <mostafal@ejada.com>
2025-04-06 23:44:29 +0200

    Add The Application and Infrastructure Services to Services Collection.

---
+++ Restaurants.Application/ServiceCollectionExtensions.cs
@@ -0,0 +1,14 @@
+﻿
+using Microsoft.Extensions.DependencyInjection;
+using Restaurants.Application.Services;
+
+namespace Restaurants.Application;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    {
+        services.AddScoped<IRestaurantsService, RestaurantsService>();
+        return services;
+    }
+}
---
+++ Restaurants.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Restaurants.Domain.Repositories;
+using Restaurants.Infrastructure.Persistence;
+using Restaurants.Infrastructure.Persistence.Repositories;
+using Restaurants.Infrastructure.Seeder;
+
+namespace Restaurants.Infrastructure.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services,IConfiguration configuration)
+    {
+        services.AddDbContext<RestaurantDbContext>(options =>
+            options.UseSqlServer(configuration.GetConnectionString("RestaurantsDb")));
+
+        services.AddScoped<IRestaurantDataSeeder, RestaurantDataSeeder>();
+
+        services.AddScoped<IRestaurantsRepository,RestaurantsRepository>();
+        
+        
+        return services;
+    }
+}
