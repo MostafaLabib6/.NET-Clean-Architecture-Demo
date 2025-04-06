2b44171899c53f7eef38b168cc56a9eeffe964a0
Mostafa Labib <mostafal@ejada.com>
2025-04-06 23:42:38 +0200

    Add Repository pattern for each entity.

---
+++ Restaurants.Domain/Repositories/RestaurantsRepository.cs
@@ -0,0 +1,9 @@
+﻿using Restaurants.Domain.Entities;
+
+namespace Restaurants.Domain.Repositories;
+
+public interface IRestaurantsRepository
+{
+    public Task<IEnumerable<Restaurant>> GetAllAsync();
+    public Task<Restaurant?> GetByIdAsync(int id);
+}
---
+++ Restaurants.Infrastructure/Persistance/Repositories/RestaurantsRepository.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Restaurants.Domain.Entities;
+using Restaurants.Domain.Repositories;
+using Restaurants.Infrastructure.Persistence;
+
+namespace Restaurants.Infrastructure.Persistence.Repositories;
+
+internal class RestaurantsRepository(RestaurantDbContext dbContext) : IRestaurantsRepository
+{
+    public async Task<IEnumerable<Restaurant>> GetAllAsync()
+    {
+        return await dbContext.Restaurants.ToListAsync();
+    }
+
+    public async Task<Restaurant?> GetByIdAsync(int id)
+    {
+        return await dbContext.Restaurants.FirstOrDefaultAsync(x => x.Id == id);
+    }
+}
