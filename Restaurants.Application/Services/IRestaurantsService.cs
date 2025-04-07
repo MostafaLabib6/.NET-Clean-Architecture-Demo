c65d743cbbf9e49cd00fdc4cd60fe4fd3a818dbd
Mostafa Labib <mostafal@ejada.com>
2025-04-08 00:19:21 +0200

    Comment Restaurant Service

--- Restaurants.Application/Services/IRestaurantsService.cs
+++ Restaurants.Application/Services/IRestaurantsService.cs
@@ -1,11 +1,11 @@
-﻿using Restaurants.Application.Restaurants;
-using Restaurants.Domain.Entities;
-
-namespace Restaurants.Application.Services;
-
-public interface IRestaurantsService
-{
-    Task<IEnumerable<RestaurantDto>> GetAllRestaurants();
-    Task<RestaurantDto?> GetByIdAsync(int id);
-    Task<int> AddRestaurant(CreateRestaurantDto restaurant);
-}
+﻿// using Restaurants.Application.Restaurants;
+// using Restaurants.Domain.Entities;
+//
+// namespace Restaurants.Application.Services;
+//
+// public interface IRestaurantsService
+// {
+//     Task<IEnumerable<RestaurantDto>> GetAllRestaurants();
+//     Task<RestaurantDto?> GetByIdAsync(int id);
+//     Task<int> AddRestaurant(CreateRestaurantDto restaurant);
+// }
--- Restaurants.Application/Services/RestaurantsService.cs
+++ Restaurants.Application/Services/RestaurantsService.cs
@@ -1,27 +1,27 @@
-﻿using Mapster;
-using MapsterMapper;
-using Restaurants.Application.Restaurants;
-using Restaurants.Domain.Entities;
-using Restaurants.Domain.Repositories;
-
-namespace Restaurants.Application.Services;
-
-internal class RestaurantsService(IRestaurantsRepository _restaurantsRepository,IMapper _mapper) : IRestaurantsService
-{
-    public async Task<IEnumerable<RestaurantDto>> GetAllRestaurants()
-    {
-        var restaurants =await _restaurantsRepository.GetAllAsync();
-        return restaurants.Adapt<IEnumerable<RestaurantDto>>();
-    }
-    public async Task<RestaurantDto?> GetByIdAsync(int id)
-    {
-        var restaurant =  await _restaurantsRepository.GetByIdAsync(id);
-        return restaurant.Adapt<RestaurantDto>();
-    }
-
-    public async Task<int> AddRestaurant(CreateRestaurantDto restaurant)
-    {
-        var restaurantEntity = restaurant.Adapt<Restaurant>();
-        return await _restaurantsRepository.AddRestaurant(restaurantEntity);
-    }
-}
+﻿// using Mapster;
+// using MapsterMapper;
+// using Restaurants.Application.Restaurants;
+// using Restaurants.Domain.Entities;
+// using Restaurants.Domain.Repositories;
+//
+// namespace Restaurants.Application.Services;
+//
+// internal class RestaurantsService(IRestaurantsRepository _restaurantsRepository,IMapper _mapper) : IRestaurantsService
+// {
+//     public async Task<IEnumerable<RestaurantDto>> GetAllRestaurants()
+//     {
+//         var restaurants =await _restaurantsRepository.GetAllAsync();
+//         return restaurants.Adapt<IEnumerable<RestaurantDto>>();
+//     }
+//     public async Task<RestaurantDto?> GetByIdAsync(int id)
+//     {
+//         var restaurant =  await _restaurantsRepository.GetByIdAsync(id);
+//         return restaurant.Adapt<RestaurantDto>();
+//     }
+//
+//     public async Task<int> AddRestaurant(CreateRestaurantDto restaurant)
+//     {
+//         var restaurantEntity = restaurant.Adapt<Restaurant>();
+//         return await _restaurantsRepository.AddRestaurant(restaurantEntity);
+//     }
+// }
