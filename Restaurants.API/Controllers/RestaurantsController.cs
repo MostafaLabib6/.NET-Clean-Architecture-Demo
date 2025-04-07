a0f19d896e6ff7d8160f19aa1833dd87d17becd9
Mostafa Labib <mostafal@ejada.com>
2025-04-07 23:02:39 +0200

    Add restaurant controller

--- Restaurants.API/Controllers/RestaurantsController.cs
+++ Restaurants.API/Controllers/RestaurantsController.cs
@@ -1,26 +1,36 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Restaurants.Application.Restaurants;
 using Restaurants.Application.Services;
 
 namespace Restaurants.API.Controllers;
 
 [ApiController]
 [Route("api/[controller]")]
-public class RestaurantsController(IRestaurantsService restaurantsService) : ControllerBase
+public class RestaurantsController(IRestaurantsService _restaurantsService) : ControllerBase
 {
     [HttpGet]
-    public IActionResult GetAll()
+    public async Task<IActionResult> GetAll()
     {
-        var restaurants = restaurantsService.GetAllAsync();
+        var restaurants = await _restaurantsService.GetAllRestaurants();
         return Ok(restaurants);
     }
+
     [HttpGet("{id}")]
     public async Task<IActionResult> GetById(int id)
     {
-        var restaurant = await restaurantsService.GetByIdAsync(id);
+        var restaurant = await _restaurantsService.GetByIdAsync(id);
         if (restaurant == null)
         {
             return NotFound();
         }
+
         return Ok(restaurant);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> AddRestaurant([FromBody] CreateRestaurantDto restaurant)
+    {
+        var id = await _restaurantsService.AddRestaurant(restaurant);
+        return CreatedAtAction(nameof(GetById), new { id }, null);
     }
 }
