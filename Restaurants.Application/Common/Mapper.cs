81daa841c435fd5fa6eb63aa5c9da152f4422582
Mostafa Labib <mostafal@ejada.com>
2025-04-07 22:58:49 +0200

    Add Mapster

---
+++ Restaurants.Application/Common/Mapper.cs
@@ -0,0 +1,34 @@
+﻿using Mapster;
+using Restaurants.Application.Dishes;
+using Restaurants.Application.Restaurants;
+using Restaurants.Domain.Entities;
+
+namespace Restaurants.Application.Common;
+
+public class Mapper : IRegister
+{
+    public void Register(TypeAdapterConfig config)
+    {
+        config.NewConfig<Restaurant, RestaurantDto>()
+            .Map(dest => dest.City, src => src.Address == null ? null : src.Address.City)
+            .Map(dest => dest.Street, src => src.Address == null ? null : src.Address.Street)
+            .Map(dest => dest.PostalCode, src => src.Address == null ? null : src.Address.PostalCode);
+
+
+        config.NewConfig<Dish, DishDto>();
+
+
+        // config.NewConfig<CreateRestaurantDto, Restaurant>()
+        //     .Map(dest => dest.Address.City, src => src.City)
+        //     .Map(dest => dest.Address.Street, src => src.Street)
+        //     .Map(dest => dest.Address.PostalCode, src => src.PostalCode);
+
+        config.NewConfig<CreateRestaurantDto, Restaurant>()
+            .Map(dest => dest.Address, src => new Address()
+            {
+                City = src.City,
+                Street = src.Street,
+                PostalCode = src.PostalCode
+            });
+    }
+}
---
+++ Restaurants.Application/Restaurants/RestaurantDto.cs
@@ -0,0 +1,17 @@
+﻿using Restaurants.Application.Dishes;
+
+namespace Restaurants.Application.Restaurants;
+
+public record RestaurantDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = default!;
+    public string Description { get; set; } = default!;
+    public string Category { get; set; } = default!;
+
+    public bool HasDelivery { get; set; }
+
+    public string? City { get; set; }
+    public string? Street { get; set; }
+    public string? PostalCode { get; set; }
+}
