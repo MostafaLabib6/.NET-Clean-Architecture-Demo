67cfb686e951d3dc259ef81f8073b65cc6ab4ce3
Mostafa Labib <mostafal@ejada.com>
2025-04-09 14:18:28 +0200

    Add middleware to log the requests that takes more than 4 seconds.

---
+++ Restaurants.API/Middelwares/RequestTimeLoggingMiddleware.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics;
+
+namespace Restaurants.API.Middlewares;
+
+public class RequestTimeLoggingMiddleware(ILogger<RequestTimeLoggingMiddleware> logger) : IMiddleware
+{
+    public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+    {
+        var startTime = Stopwatch.StartNew();
+        await next.Invoke(context);
+        startTime.Stop();
+
+        if (startTime.ElapsedMilliseconds / 1000 > 4)
+            logger.LogWarning($"RequestTime: {startTime.ElapsedMilliseconds / 1000} seconds with {context.Request.Method} {context.Request.Path}");
+    }
+}
