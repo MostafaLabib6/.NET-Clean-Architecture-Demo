993f95175841e81d03f665e9b9de6f9aa5bb386a
Mostafa Labib <mostafal@ejada.com>
2025-04-09 14:21:23 +0200

    register TimeLogging and Global ErrorHandling middlewares.

--- Restaurants.API/Program.cs
+++ Restaurants.API/Program.cs
@@ -1,8 +1,8 @@
+using Restaurants.API.Middlewares;
 using Restaurants.Application;
 using Restaurants.Infrastructure.Extensions;
 using Restaurants.Infrastructure.Seeder;
 using Serilog;
-using Serilog.Events;using Serilog.Formatting.Compact;using Serilog.Formatting.Json;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,19 +13,23 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddScoped<ErrorHandlerMiddleware>();
+builder.Services.AddScoped<RequestTimeLoggingMiddleware>();
+
 builder.Services.AddApplicationServices();
 builder.Services.AddInfrastructureServices(builder.Configuration);
 
-builder.Host.UseSerilog((context, cfg) =>
-{
-    cfg.ReadFrom.Configuration(context.Configuration);
-});
+builder.Host.UseSerilog((context, cfg) => { cfg.ReadFrom.Configuration(context.Configuration); });
 
 var app = builder.Build();
 
 var scope = app.Services.CreateScope();
 var seeder = scope.ServiceProvider.GetRequiredService<IRestaurantDataSeeder>();
 await seeder.SeedAsync();
+
+app.UseMiddleware<RequestTimeLoggingMiddleware>();
+app.UseMiddleware<ErrorHandlerMiddleware>();
+app.UseSerilogRequestLogging();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
