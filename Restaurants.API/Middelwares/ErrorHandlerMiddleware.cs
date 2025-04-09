addd88a8c55844258cb267f74d20a0378a0b20d6
Mostafa Labib <mostafal@ejada.com>
2025-04-09 14:17:46 +0200

    Add global error handling middleware

---
+++ Restaurants.API/Middelwares/ErrorHandlerMiddleware.cs
@@ -0,0 +1,26 @@
+﻿using Restaurants.Domain.Exceptions;
+
+namespace Restaurants.API.Middlewares;
+
+public class ErrorHandlerMiddleware(ILogger<ErrorHandlerMiddleware> logger) : IMiddleware
+{
+    public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+    {
+        try
+        {
+            await next.Invoke(context);
+        }
+        catch (NotFoundException ex)
+        {
+            logger.LogError(ex, ex.Message);
+            context.Response.StatusCode = StatusCodes.Status404NotFound;
+            await context.Response.WriteAsJsonAsync(new { Message = ex.Message });
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, ex.Message);
+            context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+            await context.Response.WriteAsJsonAsync(new { Message = "An error occurred while processing your request." });
+        }
+    }
+}
---
+++ Restaurants.Domain/Exceptions/NotFoundException.cs
@@ -0,0 +1,3 @@
+﻿namespace Restaurants.Domain.Exceptions;
+
+public class NotFoundException(string resourceType, string resourceId) : Exception($"The {resourceType} with id {resourceId} was not found.");
