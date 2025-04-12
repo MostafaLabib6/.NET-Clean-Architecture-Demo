5f0977c242f443f6810953769fe066fd25ac21b3
Mostafa Labib <mostafal@ejada.com>
2025-04-12 16:42:54 +0200

    Register dish repository.

--- Restaurants.Infrastructure/Extensions/ServiceCollectionExtensions.cs
+++ Restaurants.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Restaurants.Domain.Entities;
 using Restaurants.Domain.Repositories;
 using Restaurants.Infrastructure.Persistance.Repositories;
 using Restaurants.Infrastructure.Persistence;
@@ -10,15 +11,18 @@ namespace Restaurants.Infrastructure.Extensions;
 
 public static class ServiceCollectionExtensions
 {
-    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services,IConfiguration configuration)
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
     {
         services.AddDbContext<RestaurantDbContext>(options =>
             options.UseSqlServer(configuration.GetConnectionString("RestaurantsDb")));
 
         services.AddScoped<IRestaurantDataSeeder, RestaurantDataSeeder>();
 
-        services.AddScoped<IRestaurantsRepository,RestaurantsRepository>();
-        
+        services.AddScoped<IRestaurantsRepository, RestaurantsRepository>();
+        services.AddScoped<IDishesRepository, DishesRepository>();
+
+        services.AddIdentityApiEndpoints<User>()
+            .AddEntityFrameworkStores<RestaurantDbContext>();
         
         return services;
     }
